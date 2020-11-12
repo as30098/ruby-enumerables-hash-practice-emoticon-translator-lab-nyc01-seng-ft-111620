fd55ea5bc77422415f090413fb8309fb0beac560
Alexandra Spencer <alexspencer421@gmail.com>
2020-11-12 15:39:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,8 +15,8 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   hash_translations = load_library(file_path)
-    hash_translations.keys.find do |key| 
-       binding.pry
+  hash_translations.keys.find do |key| 
+     binding.pry
   end 
 end
 
