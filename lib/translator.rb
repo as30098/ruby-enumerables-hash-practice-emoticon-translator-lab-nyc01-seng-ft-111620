884d35e627b35b161c425ad64cba3f3659f79731
Alexandra Spencer <alexspencer421@gmail.com>
2020-11-12 15:52:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_english_meaning(file_path, emoticon)
   hash_translations = load_library(file_path)
   hash_translations.keys.find do |key| 
     if hash_translations[key][:japanese] == emoticon
-      return hash_translations[key]
+      return key
     end 
   end 
   return "Sorry, that emoticon was not found"
