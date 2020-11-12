ef0a1d42457bdd8db4fa848e977548a0fed35cbb
Alexandra Spencer <alexspencer421@gmail.com>
2020-11-12 15:48:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon(file_path, emoticon)
       return hash_translations[key][:japanese]
      end 
   end 
-  "Sorry, that emoticon was not found"
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file_path, emoticon)
